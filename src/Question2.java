eb0bdfeebe839fc92091180f333d746ef9be3c04
koala99 <lilei19891030@qq.com>
2018-12-13 17:17:45 +0800

    fix

--- src/Question2.java
+++ src/Question2.java
@@ -8,4 +8,31 @@ public class Question2 {
         int result = (n) & (n - 1);
         return result == 0;
     }
+
+    //326. 3的幂
+    public static boolean isPowerOfThree(int n) {
+        if (n <= 0)
+            return false;
+        if (n == 1)
+            return true;
+        double tmp = Math.log10(n) / Math.log10(3.0);
+        if (Math.round(tmp) - tmp == 0)
+            return true;
+        else
+            return false;
+    }
+
+    //   342. 4的幂
+    public boolean isPowerOfFour(int n) {
+//        return (num&(num-1)) ==0 && (num&0x55555555)==num;
+        if (n <= 0)
+            return false;
+        if (n == 1)
+            return true;
+        double tmp = Math.log10(n) / Math.log10(4.0);
+        if (Math.round(tmp) - tmp == 0)
+            return true;
+        else
+            return false;
+    }
 }
