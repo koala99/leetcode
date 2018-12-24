ff06d3e09c41e778120f97fe367f4d67ba4a28fd
koala99 <lilei19891030@qq.com>
2018-12-24 14:00:06 +0800

    add questions

--- src/Question2.java
+++ src/Question2.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Question2 {
     //231. 2的幂
     public static boolean isPowerOfTwo(int n) {
@@ -24,7 +26,6 @@ public class Question2 {
 
     //   342. 4的幂
     public boolean isPowerOfFour(int n) {
-//        return (num&(num-1)) ==0 && (num&0x55555555)==num;
         if (n <= 0)
             return false;
         if (n == 1)
@@ -34,5 +35,30 @@ public class Question2 {
             return true;
         else
             return false;
+    }
+
+    //709. 转换成小写字母
+    public static String toLowerCase(String str) {
+        if (null == str) {
+            return null;
+        }
+        char ch[] = str.toCharArray();
+        for (int i = 0; i < ch.length; i++) {
+            if (ch[i] >= 'A' && ch[i] <= 'Z') {
+                ch[i] += 32;
+            }
+        }
+        String s = new String(ch);
+        return s;
+    }
+
+    //961. 重复 N 次的元素
+    public static int repeatedNTimes(int[] A) {
+        Arrays.sort(A);
+        for (int i = 1; i < A.length; i++) {
+            if (A[i] == A[i - 1])
+                return A[i];
+        }
+        return 0;
     }
 }
