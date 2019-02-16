48660d444ff33bc82c7a5d351a8011aa5502a4ea
koala99 <lilei19891030@qq.com>
2019-02-16 13:27:08 +0800

    fix

--- src/sum/TwoSum.java
+++ src/sum/TwoSum.java
@@ -3,9 +3,7 @@ package sum;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * Given nums = [2, 7, 11, 15], target = 9,Because nums[0] + nums[1] = 2 + 7 = 9,return [0, 1].
- */
+//1. 两数之和
 public class TwoSum {
 
     public int[] twoSum1(int[] nums, int target) {
@@ -26,6 +24,19 @@ public class TwoSum {
             }
         }
         return array;
+    }
+
+    //
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
     }
 
     public static void main(String args[]) {
