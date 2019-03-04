e100ee8d4fc83f57e1fcee606a1f0ce8290053a0
koala99 <lilei19891030@qq.com>
2019-03-04 20:24:47 +0800

    add files

--- src/Test.java
+++ src/Test.java
@@ -2,9 +2,8 @@ public class Test {
     public static void main(String[] args) {
 //        System.out.println(Question1.numJewelsInStones("aaA", "aAbbb"));
 //          System.out.println(Question2.isPowerOfThree(0));
-//        int[] arrays = {2, 2, 4, 6};
 //        Question1.singleNumber3(arrays);
-          System.out.println(Question2.toLowerCase("AbssS"));
+//          System.out.println(Question2.toLowerCase("AbssS"));
 
     }
 }
